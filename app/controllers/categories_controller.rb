a1e107cde03c69ff4968495e59d7efdcd75aa109
Bohdan <lsdovscky@gmail.com>
2018-06-01 15:10:34 +0300

    add admin namespace

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,38 +1,10 @@
 class CategoriesController < ApplicationController
-  before_action :admin_user, only: [:new, :create]
   before_action :set_category, only: :show
 
   def show
   end
 
-  def new
-    @category = Category.new
-  end
-
-  def create
-    @category = Category.new(category_params)
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to root_path, notice: 'Новая категория!' }
-      else
-        format.html { render :new }
-      end
-    end
-  end
-
   private
-
-  def category_params
-    params.require(:category).permit(:name, :parent_mood)
-  end
-
-  def admin_user
-    if current_user.present? && current_user.role == 'admin'
-      true
-    else
-      redirect_to root_path
-    end
-  end
 
   def set_category
     @category = Category.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users, controllers: { sessions: 'user/sessions'}
+
   resources :posts do
     collection do
       get 'fresh'
     end
   end
+
   resources :profiles, only: :show
-  resources :categories, only: [:index, :show]
+  resources :categories, only: :show
   resources :tags, only: :show
 
-  scope :admin, as: 'admin' do
-    resources :categories, only: [:new, :create]
+  namespace :admin do
+    resources :categories, except: [:show, :index]
   end
 end
